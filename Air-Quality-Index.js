15ba8f74f55cb9c3e973642e41593e1783845e40
Shubham Singh <84686236+Darkkingshubh@users.noreply.github.com>
2024-04-30 18:35:11 +0100

    Add files via upload

---
+++ Air-Quality-Index.js
@@ -0,0 +1,22 @@
+// Air Quality Index 💨
+// Codédex
+const maxaqi = 350;
+const minaqi = 0;
+
+let aqis = Math.random();
+let aqi = Math.floor(aqis * (maxaqi - minaqi + 1)) + minaqi;
+
+
+if (aqi >= 0 && aqi <= 50) {
+  console.log("Good");
+} else if (aqi >= 51 && aqi <= 100) {
+  console.log("Moderate");
+} else if (aqi >= 101 && aqi <= 150) {
+  console.log("Unhealthy (Sensitive Groups)");
+} else if (aqi >= 151 && aqi <= 200) {
+  console.log("Unhealthy");
+} else if (aqi >= 201 && aqi <= 300) {
+  console.log("Very Unhealthy");
+} else {
+  console.log("Hazardous");
+}
